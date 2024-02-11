3c1ae73da3b5943475303309dc1f731d999500af
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-12 01:44:21 +0400

    Tours remove Tour

--- src/Tours.js
+++ src/Tours.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Tour from "./Tour";
-const Tours = ({ tours }) => {
+const Tours = ({ tours, removeTour }) => {
   return (
     <section>
       <div className="title">
@@ -9,7 +9,7 @@ const Tours = ({ tours }) => {
       </div>
       <div>
         {tours.map((tour) => (
-          <Tour {...tour} key={tour.id} />
+          <Tour {...tour} key={tour.id} removeTour={removeTour} />
         ))}
       </div>
     </section>
